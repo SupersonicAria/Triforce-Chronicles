b68fa558bbe9c30eda56b49897508f5cad10b39b
unknown <tavoywalls@gmail.com>
2022-06-22 18:53:53 -0400

    added attack and magic functionality to ganon

--- ganon.js
+++ ganon.js
@@ -1,7 +1,7 @@
 let ganon = {
     hp: 400,
     strength: 20,
-    magic: 18,
+    magic: 16,
     luck: 0.03
 }
 let uncle = {
@@ -12,6 +12,7 @@ let uncle = {
 
 let ganonHp
 let uncleHp
+let playerTurn = true
 
 let setupGame = function(){
     ganonHp = document.getElementById('playerHp')
@@ -19,6 +20,54 @@ let setupGame = function(){
 
     uncleHp = document.getElementById('enemyHp')
     uncleHp.textContent = `Enemy Hp:${uncle.hp}`
+}
+
+let attack = function(){
+    if (uncle.hp >= 1 && playerTurn){
+        //from codegrepper how to choose number between 1 and 6
+        let roll = Math.floor(Math.random() * 6) + 1;
+        let dmgCalc = roll/2
+        let appliedDmg = dmgCalc * ganon.strength
+        
+        let luckRoll = ganon.luck * Math.floor(Math.random() * 33.4)
+        if (luckRoll >= 1) {
+            appliedDmg = appliedDmg * 2.5
+            console.log("CRIT")
+        }
+        
+        
+        
+        uncle.hp = uncle.hp - appliedDmg
+        uncleHp = document.getElementById('enemyHp')
+        uncleHp.textContent = `Enemy Hp:${uncle.hp}`
+    }
+}
+
+let magic = function(){
+    if (uncle.hp >= 1 && playerTurn){
+        let roll = Math.floor(Math.random() * 6) + 1;
+        let dmgCalc = roll/2
+        let appliedDmg = dmgCalc * ganon.magic
+        
+        
+        
+        let luckRoll = ganon.luck * Math.floor(Math.random() * 33.4) * 1.2
+        if (luckRoll >= 1) {
+            appliedDmg = appliedDmg * 2.5
+            console.log("CRIT")
+        }
+
+        let integerCheck = appliedDmg % 1
+        if (integerCheck != 0) {
+            appliedDmg = appliedDmg + 0.5
+        }
+        
+        
+        
+        uncle.hp = uncle.hp - appliedDmg
+        uncleHp = document.getElementById('enemyHp')
+        uncleHp.textContent = `Enemy Hp:${uncle.hp}`
+    }
 }
     
 
