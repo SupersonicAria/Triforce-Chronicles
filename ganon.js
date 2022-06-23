096c9f41d5a56fa7530de5ed15e0ef2684d41638
unknown <tavoywalls@gmail.com>
2022-06-22 21:13:04 -0400

    cleaned up code and set groundwork for enemy turn

--- ganon.js
+++ ganon.js
@@ -10,12 +10,13 @@ let uncle = {
     magic: 12,
     defense: 0.5
 }
-
 let ganonHp
 let uncleHp
-let playerTurn = true
+let enemyHp
 let shade
 let shadeCount
+let crit = false
+let playerTurn = true
 
 let setupGame = function () {
     ganonHp = document.getElementById('playerHp')
@@ -27,146 +28,129 @@ let setupGame = function () {
 
 let attack = function () {
     if (uncle.hp >= 1 && playerTurn) {
-        //from codegrepper how to choose number between 1 and 6
-        let roll = Math.floor(Math.random() * 6) + 1;
-        let dmgCalc = roll / 2
-        let appliedDmg = dmgCalc * ganon.strength
-
-        let luckRoll = ganon.luck * Math.floor(Math.random() * 33.4)
-        if (luckRoll >= 1) {
-            appliedDmg = appliedDmg * 2.5
-            console.log("CRIT")
-        }
-
-
-        
+        dealDamagePlayer(2, ganon, 'Ganon', ganon.strength, 1, true, 1, uncle)
 
         if (shade) {
             shadeAttack()
         }
 
-        appliedDmg = appliedDmg * uncle.defense
-
-        let integerCheck = appliedDmg % 1
-        if (integerCheck != 0) {
-            appliedDmg = Math.round(appliedDmg)
-        }
-
-        uncle.hp = uncle.hp - appliedDmg
-        uncleHp = document.getElementById('enemyHp')
-        uncleHp.textContent = `Enemy Hp:${uncle.hp}`
+        passTurn()
     }
 }
 
 let magic = function () {
     if (uncle.hp >= 1 && playerTurn) {
-        let roll = Math.floor(Math.random() * 6) + 1;
-        let dmgCalc = roll / 2
-        let appliedDmg = dmgCalc * ganon.magic
-
-
-
-        let luckRoll = ganon.luck * Math.floor(Math.random() * 33.4) * 1.2
-        if (luckRoll >= 1) {
-            appliedDmg = appliedDmg * 2.5
-            console.log("CRIT")
-        }
+        dealDamagePlayer(2, ganon, 'Ganon', ganon.magic, 1, true, 1.2, uncle)
 
         if (shade) {
             shadeAttack()
         }
 
-        
-
-        appliedDmg = appliedDmg * uncle.defense
-
-        let integerCheck = appliedDmg % 1
-        if (integerCheck != 0) {
-            appliedDmg = Math.round(appliedDmg)
-        }
-
-        uncle.hp = uncle.hp - appliedDmg
-        uncleHp = document.getElementById('enemyHp')
-        uncleHp.textContent = `Enemy Hp:${uncle.hp}`
-
-        console.log("attack no shade: ", appliedDmg)
+        passTurn()
     }
 }
 
 let fist = function () {
     if (uncle.hp >= 1 && playerTurn) {
-        let roll = Math.floor(Math.random() * 6) + 1;
-        let dmgCalc = roll / 3
-        let appliedDmg = dmgCalc * ganon.strength * 1.5
-
-        let luckRoll = ganon.luck * Math.floor(Math.random() * 33.4)
-        if (luckRoll >= 1) {
-            appliedDmg = appliedDmg * 2.5
-            console.log("CRIT")
-        }
-
-
-        
+        dealDamagePlayer(2.5, ganon, 'Ganon', ganon.strength, 1.5, true, 1, uncle)
 
         if (shade) {
             shadeAttack()
         }
 
-        appliedDmg = appliedDmg * uncle.defense
-
-        let integerCheck = appliedDmg % 1
-        if (integerCheck != 0) {
-            appliedDmg = Math.round(appliedDmg)
-        }
-
-        uncle.hp = uncle.hp - appliedDmg
-        uncleHp = document.getElementById('enemyHp')
-        uncleHp.textContent = `Enemy Hp:${uncle.hp}`
-
         uncle.defense = uncle.defense + 0.1
+        alert(`Enemy defense reduced!`)
+
+        passTurn()
     }
 }
 
-let summon = function(){
-    if (!shade){
+let summon = function () {
+    if (!shade) {
         shade = true
         shadeCount = 5
-        console.log("shade summoned")
+        alert(`Shade Summoned!`)
+        passTurn()
     }
-    
 }
 
-let shadeAttack = function(){
+let shadeAttack = function () {
     if (uncle.hp >= 1 && playerTurn) {
-        //from codegrepper how to choose number between 1 and 6
-        let roll = Math.floor(Math.random() * 6) + 1;
-        let dmgCalc = roll / 4
-        let shadeDmg = dmgCalc * ganon.strength
 
-
-        shadeDmg = shadeDmg * uncle.defense
-
-        let integerCheck = shadeDmg % 1
-        if (integerCheck != 0) {
-            shadeDmg = Math.round(shadeDmg)
-        }
-
-        uncle.hp = uncle.hp - shadeDmg
-        uncleHp = document.getElementById('enemyHp')
-        uncleHp.textContent = `Enemy Hp:${uncle.hp}`
+        dealDamagePlayer(4, ganon, 'Shade', ganon.strength, 1, false, 1, uncle)
 
         if (shadeCount == 1) {
             shade = false
         }
 
         shadeCount = shadeCount - 1
-        console.log("shade", shadeDmg)
         console.log("shade count", shadeCount)
-
-        
     }
 }
 
+let passTurn = function () {
+    let attackRoll = Math.floor(Math.random() * 5) + 1;
+
+    if (attackRoll == 1 || attackRoll == 2) {
+        enemyAttack()
+    }
+    if (attackRoll == 3 || attackRoll == 4) {
+        enemyMagic()
+    }
+    if (attackRoll == 5) {
+        uncleSpecial()
+    }
+}
+
+let enemyAttack = function () {
+    alert(`att!`)
+}
+
+let enemyMagic = function () {
+    alert(`mag`)
+}
+
+let uncleSpecial = function () {
+    alert(`spe`)
+}
+
+let dealDamagePlayer = function (odds, player, playerName, stat, statBonus, luckCheck, luckOdds, enemy) {
+    //from codegrepper how to choose number between 1 and 6
+    let roll = Math.floor(Math.random() * 6) + 1;
+    let dmgCalc = roll / odds
+    let appliedDmg = dmgCalc * stat * statBonus
+
+    let isLuck = luckCheck
+    if (isLuck) {
+        let luckRoll = player.luck * Math.floor(Math.random() * 33.4) * luckOdds
+        if (luckRoll >= 1) {
+            appliedDmg = appliedDmg * 2.5
+            console.log("CRIT")
+            crit = true
+        }
+    }
+
+    appliedDmg = appliedDmg * enemy.defense
+
+    let integerCheck = appliedDmg % 1
+    if (integerCheck != 0) {
+        appliedDmg = Math.round(appliedDmg)
+    }
+
+    let trueDmg = enemy.hp - appliedDmg
+    let startHp = enemy.hp
+    enemy.hp = trueDmg
+    enemyHp = document.getElementById('enemyHp')
+    enemyHp.textContent = `Enemy Hp:${enemy.hp}`
+
+    let dmgTaken = startHp - trueDmg
+    if (!crit) {
+        alert(`${playerName} dealt ${dmgTaken} damage!`)
+    } else {
+        alert(`${playerName} dealt ${dmgTaken} critical damage!`)
+        crit = false
+    }
+}
 
 
 
